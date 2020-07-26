3e7cc002cf9882e73233f7390f3f9095a7a44e18
mirrorming <mirrorming@qq.com>
2020-07-27 04:42:57 +0800

    ✔✔✔

--- src/main/java/cn/mirrorming/blog/service/TokenService.java
+++ src/main/java/cn/mirrorming/blog/service/TokenService.java
@@ -2,6 +2,7 @@ package cn.mirrorming.blog.service;
 
 import cn.mirrorming.blog.domain.dto.TokenDTO;
 import cn.mirrorming.blog.exception.UserException;
+import cn.mirrorming.blog.security.properties.SecurityProperties;
 import cn.mirrorming.blog.service.base.AbstractBaseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,14 +29,9 @@ public class TokenService extends AbstractBaseService {
 
     @Value("${server.port}")
     private String port;
-    @Value("${system.oauth2.grant_type}")
-    public String oauth2GrantType;
 
-    @Value("${system.oauth2.client_id}")
-    public String oauth2ClientId;
-
-    @Value("${system.oauth2.client_secret}")
-    public String oauth2ClientSecret;
+    @Autowired
+    public SecurityProperties securityProperties;
 
     interface TokenRequest {
         @POST("/oauth/token")
@@ -50,7 +46,10 @@ public class TokenService extends AbstractBaseService {
         Retrofit retrofit = builderRetrofit(URL_OAUTH_TOKEN + port + "/oauth/token/");
 
         TokenRequest tokenRequest = retrofit.create(TokenRequest.class);
-        Call<TokenDTO> call = tokenRequest.getToken(username, password, oauth2GrantType, oauth2ClientId, oauth2ClientSecret);
+        Call<TokenDTO> call = tokenRequest.getToken(username, password,
+                "password",
+                securityProperties.getOauth2().getClients().get(0).getClientId(),
+                securityProperties.getOauth2().getClients().get(0).getClientSecret());
         try {
             return call.execute().body();
         } catch (IOException e) {
